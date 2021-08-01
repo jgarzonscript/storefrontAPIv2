f7715afcc3fdb9f0a1175ff6ebc4d1c34fbe58d8
John Garzon <gajohn511@gmail.com>
2021-08-01 00:26:47 -0400

    Init Commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, RequestHandler, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app: express.Application = express();
+const address: string = '0.0.0.0:3000';
+
+app.use(express.json() as RequestHandler);
+
+app.get('/', function (req: Request, res: Response) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log(`starting app on: ${address}`);
+});
