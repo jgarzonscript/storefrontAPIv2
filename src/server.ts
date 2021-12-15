990e44855389388b5fe844775eb13c389b4727b4
John Garzon <jgarzonscript@gmail.com>
2021-12-15 00:51:23 -0500

    minor update to server.js

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.get('/conninfo', (req: Request, res: Response) => {
         const environment = config.env;
         const database = config.database;
         const connectionString = config.connectionString;
-        res.json({ connectionString });
+        res.json({ connectionString, environment });
     } catch (error) {
         res.json(error);
     }
